e1de827ce1f1795db6151eaf033c340af5818874
Matthew Morek <matthew@madbit.co>
2018-08-22 22:55:19 +0100

    🚧 Add baseline VueJS skeleton

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <DonationComponent msg="Sample text"/>
+  </div>
+</template>
+
+<script>
+import DonationComponent from './components/DonationComponent.vue';
+
+export default {
+  name: 'app',
+  components: {
+    DonationComponent
+  }
+};
+</script>
+
+<style lang="scss"></style>
