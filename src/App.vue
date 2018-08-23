897fbdb98def22d7ebbcec1d8f7331c144c801af
Matthew Morek <matthew@madbit.co>
2018-08-23 12:12:24 +0100

    🚧 Integrate second component
    - pass on on donation amount to the second component using standard `vm.$emit` method;
    - transition between both components using CSS transforms and keyframe animations;

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,35 @@
   <div id="app">
     <div class="app__wrapper">
       <h1>Co-op Donate Component Test</h1>
+      <transition name="flip" mode="out-in">
         <component :is="view" @click="donationProcess" :amount="donationAmount"></component>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
 import DonationComponent from './components/DonationComponent.vue';
+import DonationComponentSuccess from './components/DonationComponentSuccess.vue';
 
 export default {
   name: 'app',
   components: {
-    DonationComponent
+    'default': DonationComponent,
+    'success': DonationComponentSuccess
+  },
+  data: function () {
+    return {
+      view: 'default',
+      donationAmount: null
+    };
+  },
+  methods: {
+    donationProcess: function (data) {
+      var _self = this;
+      _self.view = 'success';
+      _self.donationAmount = data.amount;
+    }
   }
 };
 </script>
@@ -25,5 +42,28 @@ export default {
     margin-right: auto;
     margin-top: 2em;
     margin-bottom: 2em;
+  }
+
+  .flip-enter-active {
+    perspective: 1000;
+    animation: flipIn 0.75s;
+  }
+
+  @keyframes flipIn {
+    0% {
+      transform: perspective(400px) rotateX(90deg);
+    }
+    
+    40% {
+      transform: perspective(400px) rotateX(-10deg);
+    }
+    
+    70% {
+      transform: perspective(400px) rotateX(5deg);
+    }
+    
+    100% {
+      transform: perspective(400px) rotateX(0deg);
+    }
   }
 </style>
